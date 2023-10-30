834cca3e26f9e6ab35e3b4eaabe87d7495444823
kyo <hoang.vx.github@outlook.jp>
2023-10-30 04:22:05 +0000

    fix bug 2

--- project/starter-code/zone1/alb.tf
+++ project/starter-code/zone1/alb.tf
@@ -4,4 +4,5 @@
    account            = data.aws_caller_identity.current.account_id
    public_subnet_ids  = module.vpc.public_subnet_ids
    vpc_id             = module.vpc.vpc_id
+   ec2                = module.project_ec2.aws_instance
  }
--- project/starter-code/zone1/modules/alb/_var.tf
+++ project/starter-code/zone1/modules/alb/_var.tf
@@ -1,6 +1,7 @@
 variable "name" {}
 variable "public_subnet_ids" {}
 variable "vpc_id" {}
+variable "ec2" {}
 variable "tags" {
    default = {}
  }
